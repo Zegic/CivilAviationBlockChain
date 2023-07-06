8435ae91385231bb0c4d5a2d80fc5678e094c5f2
Zegic_gogo <36985470+CHA123123123@users.noreply.github.com>
2023-07-06 14:37:49 +0800

    依赖项错误

--- CABC_Java/src/Test_pack/Chaincode_test.java
+++ CABC_Java/src/Test_pack/Chaincode_test.java
@@ -5,18 +5,16 @@ import org.hyperledger.fabric.contract.ContractInterface;
 import org.hyperledger.fabric.shim.ChaincodeException;
 import org.hyperledger.fabric.shim.ChaincodeStub;
 import com.owlike.genson.Genson;
-
+import java.util.List;
+import org.everit.json.*;
 
 public class Chaincode_test implements ContractInterface {
     private final Genson genson = new Genson();
 
-    // 实现智能合约接口的invoke方法
     public void invoke(Context ctx) {
         ChaincodeStub stub = ctx.getStub();
-        // 获取调用的函数名称
-        String function = stub.getFunction();
 
-        // 根据函数名称进行不同的处理逻辑
+        String function = stub.getFunction();
         switch (function) {
             case "storePoints":
                 storePoints(ctx, stub.getParameters());
@@ -25,49 +23,40 @@ public class Chaincode_test implements ContractInterface {
                 getPoints(ctx, stub.getParameters());
                 break;
             default:
-                // 不支持的函数名称，抛出异常
                 throw new ChaincodeException("Unsupported function: " + function);
         }
     }
 
-    // 存储积分到区块链状态中
-    private void storePoints(Context ctx, String[] args) {
+    private void storePoints(Context ctx, List<String> args) {
         ChaincodeStub stub = ctx.getStub();
 
-        if (args.length != 2) {
-            // 参数不正确，抛出异常
+        if (args.size() != 2) {
             throw new ChaincodeException("Incorrect number of arguments. Expecting 2.");
         }
 
-        String userId = args[0];
-        int points = Integer.parseInt(args[1]);
+        String userId = args.get(0);
+        int points = Integer.parseInt(args.get(1));
 
-        // 将积分存储到区块链状态中
         stub.putStringState(userId, String.valueOf(points));
 
         System.out.println("Points stored successfully!");
     }
 
-    // 从区块链状态中获取用户积分
-    private void getPoints(Context ctx, String[] args) {
+    private void getPoints(Context ctx, List<String> args) {
         ChaincodeStub stub = ctx.getStub();
 
-        if (args.length != 1) {
-            // 参数不正确，抛出异常
+        if (args.size() != 1) {
             throw new ChaincodeException("Incorrect number of arguments. Expecting 1.");
         }
 
-        String userId = args[0];
+        String userId = args.get(0);
 
-        // 从区块链状态中获取用户积分
         String points = stub.getStringState(userId);
 
         System.out.println("Points for user " + userId + ": " + points);
     }
 
-    // 主方法，用于启动智能合约
     public static void main(String[] args) {
-        // 启动智能合约
-        new Chaincode_test().start(args);
+        new Chaincode_test().invoke(new Context(null));
     }
 }
